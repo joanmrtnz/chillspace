c9056280c56b631791969bcb875377963bf143d6
xuanor <joancomic.jm@gmail.com>
2025-04-02 16:25:10 +0200

    style(products): add more chill styles in products section

--- src/app/products/ProductCard.tsx
+++ src/app/products/ProductCard.tsx
@@ -18,10 +18,10 @@ export default function ProductCard({
   price: string;
 }) {
   return (
-    <div className="w-full backdrop-blur-lg bg-[var(--white)]/5 rounded-xl p-4 sm:p-4 shadow-lg">
-      <img src={image} alt={title} width={200} height={200} className="rounded-lg mx-auto" />
+    <div className="min-h-[60vh] w-full backdrop-blur-lg bg-[var(--white)]/5 rounded-xl p-4 sm:p-4 shadow-lg">
+      <img src={image} alt={title} width={200} height={200} className="w-full rounded-md m-0" />
       <h2 className="text-xl font-bold mt-4">{title}</h2>
-      <p className="text-sm mt-2">{description}</p>
+      <p className="min-h-[8vh] text-sm mt-2">{description}</p>
       <p className="text-lg text-green-500 font-semibold mt-3">{price}</p>
       <Link href={productLink} target="_blank" >
         <button className="mt-4 w-full font-bold bg-[var(--green)]/90 text-white hover:bg-[var(--green)] px-4 py-2 rounded">
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -16,7 +16,7 @@ export default function ProductsPage() {
   return (
     <div className="md:min-h-screen min-h-[80vh] pt-[100px] flex flex-col items-start justify-center text-left px-6 md:px-20 m-auto bg-[var(--navy)] p-4">
       <h1 className="text-3xl font-bold mb-6 text-center">Productos Chill</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
         {currentProducts.map((product) => (
           <ProductCard
             key={product.id}
@@ -28,23 +28,24 @@ export default function ProductsPage() {
           />
         ))}
       </div>
-       <div className="flex justify-center gap-4 mt-8">
+       <div className="flex justify-center gap-4 mt-10 mb-5">
         <button
           onClick={() => setCurrentPage((prev) => Math.max(prev - 1, 1))}
           disabled={currentPage === 1}
-          className="px-4 py-2 bg-[var(--lightest-slate)] text-white rounded disabled:opacity-50"
+          className="px-4 py-2 text-[var(--green)] hover:bg-teal-700/10 bg-[var(--navy)]  border border-[var(--green)] rounded disabled:opacity-50"
         >
           Anterior
         </button>
-        <span >{currentPage} de {totalPages}</span>
+       
+        <span className="text-sm px-2 py-2 text-[var(--light-slate)]">{currentPage} de {totalPages}</span>
         <button
           onClick={() => setCurrentPage((prev) => Math.min(prev + 1, totalPages))}
           disabled={currentPage === totalPages}
-          className="px-4 py-2 bg-[var(--lightest-slate)] text-white rounded disabled:opacity-50"
+          className="px-4 py-2 text-[var(--green)] hover:bg-teal-700/10 bg-[var(--navy)]  border border-[var(--green)] rounded disabled:opacity-50"
         >
           Siguiente
         </button>
-      </div>
+        </div>
     </div>
   );
 }
