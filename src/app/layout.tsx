e433021f73dfb5bd28158c00ea0a79e339c25bd3
xuanor <joancomic.jm@gmail.com>
2025-05-10 11:19:34 +0200

    feat(layout): replace home-only background with global layout background image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import Header from "@/components/Header";
 import "./globals.css";
 import Footer from "@/components/Footer";
+import Image from "next/image";
 
 export default function RootLayout({
   children,
@@ -10,13 +11,24 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <div className="min-h-screen bg-[var(--navy)]">
-          <Header />
-          <main className="">{children}</main>
-          <Footer/> 
+        <div className="relative min-h-screen bg-black">
+          <Image
+            src="/images/home.jpg"
+            alt="mountain"
+            fill
+            className="object-cover z-0"
+            priority
+          />
+
+          <div className="absolute inset-0 bg-white/20 z-10" />
+          <div className="relative z-20">
+            <Header />
+            <main className="">{children}</main>
+            <Footer />
+          </div>
         </div>
       </body>
-    </html>
+    </html >
   );
 }
 
