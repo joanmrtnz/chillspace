981b3e156af00d9ef7dd9e07d755f27c24b10710
xuanor <joancomic.jm@gmail.com>
2025-05-14 20:26:33 +0200

    feat(header): add mobile dropdown menu and update styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,25 +1,92 @@
 "use client";
-
+import { useState, useEffect } from "react";
 import Link from "next/link";
+import { FiMenu, FiX } from "react-icons/fi";
 
 export default function Header() {
-    return (
+  const [menuOpen, setMenuOpen] = useState(false);
 
-        <header className="bg-[var(--color-gray-100)] bg-opacity-30 sm:backdrop-blur-lg sm:bg-[var(--color-gray-100)]/40 fixed top-0 w-full z-50 px-6 md:px-[50px] ">
-            <nav className="h-[80px] w-full flex items-center justify-between  font-semibold text-s/7">
-                <Link href="/"><img src="/chillspace-logo.png" className="h-[8rem] md:h-[10rem] mt-2 "/></Link>
-                <ul className="hidden md:flex items-center space-x-4 ml-auto ">
-                    <Link href="/playlists" className="text-[var(--color-slate-400)] hover:text-[var(--color-text-primary)] px-5 py-2.5">Playlists</Link>
-                    <Link href="/chillroom" className="text-[var(--color-slate-400)] hover:text-[var(--color-text-primary)] px-5 py-2.5">Chill Room</Link>
-                    <Link href="/products" className="text-[var(--color-slate-400)] hover:text-[var(--color-text-primary)] px-5 py-2.5">Productos</Link>
-                    <Link href="/spotify-login">
-                        <button className="text-[var(--color-text-primary)] hover:text-[var(--color-slate-700)]/80 bg-opacity-40 bg-[var(--color-gray-100)]/30 hover:bg-[var(--color-slate-200)]/20  border hover:border-[var(--color-text-primary_20)] border-[var(--color-text-primary)] focus:ring-[var(--color-green-500)] focus:outline-none  rounded-lg text-sm px-5 py-3.5">
-                            Iniciar sesión en Spotify
-                        </button>
-                    </Link>
-                </ul>
-            </nav>
-        </header>
+  useEffect(() => {
+    document.body.classList.toggle("overflow-hidden", menuOpen);
+  }, [menuOpen]);
 
-    );
+  return (
+        <header className="bg-[var(--color-gray-100)]/90 md:bg-[var(--color-gray-100)]/30 bg-opacity-90 md:bg-opacity-30 sm:backdrop-blur-lg sm:bg-[var(--color-gray-100)]/40 fixed top-0 w-full z-50 px-6 md:px-[50px] ">
+        <nav className="h-[80px] w-full flex items-center justify-between text-s/7">
+
+        <Link href="/">
+          <img
+            src="/chillspace-logo.png"
+            alt="chillspace logo"
+            className="h-[2.5rem] my-2"
+          />
+        </Link>
+
+        <ul className="hidden md:flex items-center space-x-1 ml-auto">
+          <Link href="/playlists" className="text-[var(--color-slate-400)] hover:text-[var(--color-text-primary)] px-5 py-2.5">
+            Playlists
+          </Link>
+          <Link href="/chillroom" className="text-[var(--color-slate-400)] hover:text-[var(--color-text-primary)] px-5 py-2.5">
+            Chillroom
+          </Link>
+          <Link href="/products" className="text-[var(--color-slate-400)] hover:text-[var(--color-text-primary)] px-5 py-2.5">
+            Productos
+          </Link>
+          <Link href="/about" className="text-[var(--color-slate-400)] hover:text-[var(--color-text-primary)] px-5 py-2.5">
+            Proposito
+          </Link>
+
+          <Link href="/spotify-login">
+            <button className="text-[var(--color-text-primary)] hover:text-[var(--color-slate-700)]/80 bg-opacity-40 
+            bg-[var(--color-gray-100)]/30 hover:bg-[var(--color-slate-200)]/20  border hover:border-[var(--color-text-primary_20)] 
+            border-[var(--color-text-primary)] focus:ring-[var(--color-green-500)] focus:outline-none  rounded-lg text-sm px-5 py-3.5">Iniciar sesión en Spotify</button>
+          </Link>
+        </ul>
+
+        <button
+          onClick={() => setMenuOpen(true)}
+          className="md:hidden text-3xl text-[var(--color-text-primary)]"
+          aria-label="Abrir menú">
+          <FiMenu />
+        </button>
+      </nav>
+
+      <div
+        className={`font-mono fixed inset-0 bg-[var(--color-gray-100)]/60 backdrop-blur-md flex flex-col items-center justify-start gap-1 text-lg transform transition-transform duration-300
+                    ${menuOpen ? "translate-x-0" : "translate-x-full"} md:hidden`}>
+        <button
+          onClick={() => setMenuOpen(false)}
+          className="absolute top-7 right-6 text-3xl text-[var(--color-text-primary)]"
+          aria-label="Cerrar menú">
+          <FiX />
+        </button>
+        <Link href="/">
+          <img
+            src="/chillspace-logo.png"
+            alt="chillspace logo"
+            className="absolute top-5 left-4 max-h-[2.5rem]"/>
+        </Link>
+        <Link href="/spotify-login" className="mt-20 bg-black/5 text-[var(--color-slate-400)]
+         px-4 py-2 rounded-md w-full transition" onClick={() => setMenuOpen(false)}>
+            Iniciar sesión en Spotify
+        </Link>
+        <Link href="/playlists" className="bg-white/30 text-[var(--color-slate-400)]
+         px-4 py-2 rounded-md w-full transition" onClick={() => setMenuOpen(false)}>
+          Playlists
+        </Link>
+        <Link href="/chillroom" className="bg-white/30 text-[var(--color-slate-400)]
+         px-4 py-2 rounded-md w-full transition" onClick={() => setMenuOpen(false)}>
+          Chillroom
+        </Link>
+        <Link href="/products" className="bg-white/30 text-[var(--color-slate-400)]
+         px-4 py-2 rounded-md w-full transition" onClick={() => setMenuOpen(false)}>
+          Productos
+        </Link>
+        <Link href="/about" className="bg-white/30 text-[var(--color-slate-400)]
+         px-4 py-2 rounded-md w-full transition" onClick={() => setMenuOpen(false)}>
+          About
+        </Link>
+      </div>
+    </header>
+  );
 }
